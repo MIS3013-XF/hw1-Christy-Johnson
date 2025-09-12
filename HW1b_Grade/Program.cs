d05f79fd05acea6cf64a50f8f44230aa95d0e92f
Christy Johnson <Christy.L.Johnson-1@ou.edu>
2025-09-11 22:28:43 -0500

    Homework 1 Completed

--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1b Grade
 
-// Your Name:
+// Your Name: Christy Johnson
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -15,6 +15,54 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            
+            const double homework_weight = 0.20;
+            const double participation_weight = 0.15;
+            const double exam1_weight = 0.15;
+            const double exam2_weight = 0.25;
+            const double exam3_weight = 0.25;
+
+
+            Console.WriteLine("What is your first name? ");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("What is your last name? ");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("What is your student id? ");
+            int studentid = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for homeworks? ");
+            double homeworkgrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for participations? ");
+            double participationgrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for Exam 1? ");
+            double exam1grade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for the Exam 2? ");
+            double exam2grade = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("What is your overall percentage grade for the Exam3? ");
+            double exam3grade = Convert.ToDouble(Console.ReadLine());
+
+            double FinalGrade = (homeworkgrade * homework_weight) +
+                (participationgrade * participation_weight) + (exam1grade * exam1_weight)
+                + (exam2grade * exam2_weight) + (exam3grade * exam3_weight);
+
+
+            Console.WriteLine($"\n{firstname} {lastname} ({studentid}), your final grade is {FinalGrade:F2}%");
+            
+            Console.WriteLine("\nPress any key to continue...");
+
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
